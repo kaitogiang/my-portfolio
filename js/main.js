2e951ea7de7e87e1252f6ed62b2f6faee4d7d0c4
Giang Dai Huy <huygiang001442@gmail.com>
2025-09-12 21:17:08 +0700

    feat: validate email using RegExp

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
+const contactEmailInput = document.getElementById("email");
+const contactMessageInput = document.getElementById("message");
+const rootStyles = getComputedStyle(document.documentElement);
+const checkIcon = rootStyles.getPropertyValue("--check-circle-icon").trim();
+const wrongIcon = rootStyles.getPropertyValue("--wrong-circle-icon").trim();
+
 document.addEventListener("scroll", (event) => {
     console.log(window.scrollY);
     handleArrowUpVisibility(window.scrollY);
 });
 
-// const rootStyles = getComputedStyle(document.documentElement);
-// const checkIcon = rootStyles.getPropertyValue("--check-circle-icon").trim();
-// const wrongIcon = rootStyles.getPropertyValue("--wrong-circle-icon").trim();
-
 // console.log(checkIcon);
 // console.log(wrongIcon);
-
+setupListeners();
 /**
  *
  * @param {Number} offset
@@ -25,5 +27,28 @@ function handleArrowUpVisibility(offset) {
     } else {
         arrowUpElement.style.opacity = 1;
         console.log("Block");
+    }
+}
+
+function setupListeners() {
+    contactEmailInput.addEventListener("input", validateEmailInput);
+}
+/**
+ *
+ * @param {Event} event
+ */
+function validateEmailInput(event) {
+    const regex =
+        /[A-Za-z][\w]*(?:[._\-\+][\w]+)?@[\w]+(?:\.[\w]+)*\.[A-Za-z]{2,}/g;
+    const currentValue = event.target.value;
+    if (currentValue == "") {
+        contactEmailInput.style.backgroundImage = "";
+        return;
+    }
+    const isValidEmail = regex.test(currentValue);
+    if (isValidEmail) {
+        contactEmailInput.style.backgroundImage = checkIcon;
+    } else {
+        contactEmailInput.style.backgroundImage = wrongIcon;
     }
 }
